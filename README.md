bdb64f92ba9f388d77666a4a41349442e281588c
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-03-31 16:54:57 +0330

    code add
    String incrementer

--- README.md
+++ README.md
@@ -3021,6 +3021,27 @@
     </td>
 </tr>
 
+<!-- 144 __________________________________________________________________________________________________ -->
+
+<tr>
+    <td>
+        144
+    </td>
+    <td>
+        String incrementer
+    </td>
+    <td>
+        <a href="https://www.codewars.com/kata/54a91a4883a7de5d7800009c" target="_blank">
+            problem
+        </a>
+    </td>
+    <td>
+        <a href="https://github.com/AI-Cortex/codewars_solution/blob/main/code%20c%2B%2B/String%20incrementer.cpp" target="_blank">
+            solution c++
+        </a>
+    </td>
+</tr>
+
 <!-- . __________________________________________________________________________________________________ -->
 
 <tr>
---
+++ code c++/String incrementer.cpp
@@ -0,0 +1,24 @@
+// my profile : https://codeforces.com/profile/AI-Cortex
+// Codeforces solution github repository : https://github.com/AI-Cortex/codewars_solution
+//////////////////////////////////////////////////////////////////////
+// name : String incrementer
+// link : https://www.codewars.com/kata/54a91a4883a7de5d7800009c
+
+#include <string>
+
+std::string incrementString(const std::string &str)
+{
+
+    std::size_t len = str.size();
+    if (!isdigit(str[len - 1]))
+        return str + "1";
+
+    size_t last_index = str.find_last_not_of("0123456789");
+    std::string number = str.substr(last_index + 1);
+    len = number.size();
+    number = std::to_string(stoi(number) + 1);
+    if (len > number.size())
+        number = std::string(len - number.size(), '0') + number;
+
+    return str.substr(0, last_index) + number;
+}
